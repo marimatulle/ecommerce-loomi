d528c2636c675b963121d1e38243b89b70604740
Marina Matulle <marinamatulle.mm@gmail.com>
2025-10-16 12:05:07 -0300

    chore: adds swagger into login.dto, register.dto and create-client.dto

--- src/auth/dtos/register.dto.ts
+++ src/auth/dtos/register.dto.ts
@@ -7,21 +7,26 @@ import {
   MinLength,
 } from 'class-validator';
 import { UserRole } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @MinLength(8)
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsEnum(UserRole)
   role?: UserRole;
